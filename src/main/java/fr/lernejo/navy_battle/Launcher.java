117e8af741ffae8ffc6e6568a3ebdc79a9c0f042
Léo Tomasino <bien_sur@kali.bien-sur.local>
2023-01-10 10:35:37 +0100

    Client API update

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -6,18 +6,18 @@ import java.net.URL;
 public class Launcher
 {
     public static void main(String[] args) throws IOException, InterruptedException {
-        int port = Integer.parseInt(args[0]);
-        if (port <= 0) throw new IllegalArgumentException("Invalid port");
-        System.out.println("Starting server on port " + args[0] + "...");
-        try {
+        if (args.length == 1) {
+            int port = Integer.parseInt(args[0]);
+            if (port <= 0) throw new IllegalArgumentException("Invalid port");
+            System.out.println("Starting server on port " + args[0] + "...");
             Server.start(port);
-            System.out.println("Server started successfully !");
-            if (args.length == 2) {
-                URL url = new URL(args[1]);
-                int clientPort = url.getPort();
-                String adversaryUrl = args[1];
-                Server.start(clientPort);
-                Client.post(adversaryUrl, port, "Test message"); }
-        } catch (IOException e) { e.printStackTrace(); }
+            System.out.println("Server started successfully !"); }
+        if (args.length >= 2) {
+            int port = Integer.parseInt(args[0]);
+            if (port <= 0) throw new IllegalArgumentException("Invalid port");
+            String serverUrl = args[1];
+            Server.start(port);
+            Client.post(serverUrl, port,"Test message");
+        }
     }
 }
